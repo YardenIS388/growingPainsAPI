8778df1c0f7c0e11d6e02d9326d60db8c62f7030
yarden <yard48@gmail.com>
2023-05-08 16:59:57 +0300

    commit for deployment

---
+++ Model/story.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const StorySchema = new mongoose.Schema({
+  content: {
+    type: String,
+  },
+  hebrewContent: {
+    type: String,
+  },
+  art: {
+    type: String,
+  },
+  ageGroup: {
+    type: String,
+  },
+  audioFIleName: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// StorySchema.pre("save", function (next) {
+//   var story = this;
+//   Story.find({}, function (err, stories) {
+//     if (err) throw err;
+//     story.sno = stories.length + 1;
+//     next();
+//   });
+// });
+
+module.exports = mongoose.model("Story", StorySchema);
---
+++ router.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const {getStories, createStory, getStoryById } = require("./controllers/Story")
+
+router.get("/", (req, res) => {
+  res.send("Growing Pains");
+});
+
+router.get("/stories", getStories)
+
+router.get("/stories/story/:storyID", getStoryById )
+
+router.post("/stories", createStory);
+
+
+
+module.exports = router;
