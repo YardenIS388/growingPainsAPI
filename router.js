24958635b0e3697f363a945964cfbd5c8b83b85a
yarden <yard48@gmail.com>
2023-05-16 19:38:43 +0300

    no root

--- router.js
+++ router.js
@@ -5,9 +5,6 @@ router.get("/", (req, res) => {
   res.send("Growing Pains");
 });
 
-router.get("/", ()=> {
-  return '<h1> Growing Pains Root </h1>'
-})
 router.get("/stories", getStories)
 
 router.get("/stories/story/:storyID", getStoryById )
