3508deab889824f49e053c8d7ce35ecdf5963d57
yarden <yard48@gmail.com>
2023-05-17 18:09:54 +0300

    vercel cors handler removed

--- index.js
+++ index.js
@@ -1,67 +1,3 @@
-const express = require("express");
-const router = require("./router");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const cors = require("cors");
-
-dotenv.config();
-
-const PORT = 8000;
-
-const app = express();
-
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
-
-const handler = (req, res) => {
-  const d = new Date();
-  res.end(d.toString());
-};
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use(allowCors(handler)); // Wrap the handler with allowCors middleware
-
-app.use(router);
-
-mongoose
-  .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-app.listen(PORT, () => {
-  console.log(`Server up on port ${PORT}`);
-});
-
-
-
-
-
-
 // const express = require("express");
 // const router = require("./router");
 // const mongoose = require("mongoose");
@@ -74,13 +10,35 @@ app.listen(PORT, () => {
 
 // const app = express();
 
-// app.use((req, res, next) => {
-//   next();
-// });
+// const allowCors = (fn) => async (req, res) => {
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+//   );
+//   if (req.method === "OPTIONS") {
+//     res.status(200).end();
+//     return;
+//   }
+//   return await fn(req, res);
+// };
+
+// const handler = (req, res) => {
+//   const d = new Date();
+//   res.end(d.toString());
+// };
 
 // app.use(cors());
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
+
+// app.use(allowCors(handler)); // Wrap the handler with allowCors middleware
+
 // app.use(router);
 
 // mongoose
@@ -98,3 +56,45 @@ app.listen(PORT, () => {
 // app.listen(PORT, () => {
 //   console.log(`Server up on port ${PORT}`);
 // });
+
+
+
+
+
+
+const express = require("express");
+const router = require("./router");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const PORT = 8000;
+
+const app = express();
+
+app.use((req, res, next) => {
+  next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(router);
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server up on port ${PORT}`);
+});
